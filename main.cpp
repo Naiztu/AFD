9e2649ede6438708c91fbb8f68c226d65179a7b2
CamalichM <99758150+CamalichM@users.noreply.github.com>
2022-03-20 23:43:40 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,28 +7,29 @@ using namespace std;
 #include "simbolo.h"
 
 void lexerAritmetico(string archivo)
-{
+{ //Creación de la función principal del archivo
 
     Variable val = Variable();
     Number num = Number();
     Simbolo sim = Simbolo();
 
     enum TStatus
-    {
+    {//Cantidad de casos del autómata que relaciona la identificación de número, variable y símbolo
         q0,
         q1,
         q2,
         q3,
     };
+    //Definición de los estados, de line y de la carga del archivo
     TStatus state;
     state = q0;
     string line = "";
     fstream file = fstream(archivo);
     int numline = 1;
 
-    if (file.is_open())
+    if (file.is_open())//Lectura del archivo
         while (!file.eof())
-        {
+        { //Si el archivo no se termina, se lee cada línea del archivo para poder realizar el procedimiento, uno por uno.
             int index = 0;
             getline(file, line);
             line.size() != 0 && cout << "\nLINE " << numline << endl;
@@ -36,26 +37,26 @@ void lexerAritmetico(string archivo)
             while (index != line.size())
             {
                 switch (state)
-                {
-                case q0:
+                { //Toma de texto y observación por medio de casos
+                case q0: //Redirección
                     if (isdigit(line[index]))
-                        state = q1;
+                        state = q1; //Caso de números
                     else if (line[index] == ' ')
-                        index++;
+                        index++; //Saltar espacios
                     else if (isalpha(line[index]))
-                        state = q2;
+                        state = q2; //Caso de variables
                     else
-                        state = q3;
+                        state = q3; //Caso de símbolos
                     break;
-                case q1:
+                case q1: // Número, regresa después al análisis del caso q0
                     index = num.isNumber(line, index);
                     state = q0;
                     break;
-                case q2:
+                case q2: // Variable, regresa después al análisis del caso q0
                     index = val.isVariable(line, index);
                     state = q0;
                     break;
-                case q3:
+                case q3: // Símbolo, regresa después al análisis del caso q0
                     index = sim.isSimbolo(line, index);
                     state = q0;
                     break;
@@ -68,12 +69,12 @@ int main()
 {
 
     string file = "";
-    cout << "Nombre del archivo (con extension .txt): ";
+    cout << "Nombre del archivo (con extension .txt): "; //El programa pide el nombre del archivo con extensión, en este caso "prueba.txt"
     cin >> file;
 
     if (".txt" == file.substr(file.size() - 4))
-        lexerAritmetico(file);
+        lexerAritmetico(file); //Se ejecuta la función anterior con el archivo de texto entregado
     else
-        cout << "EL archivo tiene que ser de tipo .txt";
+        cout << "EL archivo tiene que ser de tipo .txt"; //Si no es txt, no se lee
     return 0;
 }
