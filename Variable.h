516e8faf7dba2e92f2788bf32b3132dd49066708
CamalichM <99758150+CamalichM@users.noreply.github.com>
2022-03-21 02:23:38 -0600

    Update Variable.h

--- Variable.h
+++ Variable.h
@@ -5,7 +5,7 @@
 using namespace std;
 
 class Variable
-{
+{ //Creación de clase Variable
 public:
     Variable(){};
     int isVariable(string, int);
@@ -14,7 +14,7 @@ public:
 int Variable::isVariable(string text, int index)
 {
     enum TStatus
-    {
+    { //Tres diferentes estados detectados previamente.
         q0,
         q1,
         q2
@@ -22,11 +22,11 @@ int Variable::isVariable(string text, int index)
     TStatus state;
     state = q0;
     int i = index, longitud = text.size();
-    while (state != q2)
+    while (state != q2) //Si se obtiene el q2, se acaba el while
     {
         switch (state)
         {
-        case q0:
+        case q0: //Detecta si es letra y se manda al caso q1, si es falso, es un error de la variable
             if (isalpha(text[i]))
             {
                 state = q1;
@@ -36,7 +36,7 @@ int Variable::isVariable(string text, int index)
                 state = q2;
             break;
         case q1:
-            if (isalpha(text[i]) || text[i] == '_' || isdigit(text[i]))
+            if (isalpha(text[i]) || text[i] == '_' || isdigit(text[i])) //Si es letra, guión bajo o número, se podrá reiterar hasta que esto deje de cumplirse.
                 i++;
             else
                 state = q2;
@@ -44,7 +44,7 @@ int Variable::isVariable(string text, int index)
         }
     }
     (i != index) &&
-        cout << "Variable : " << text.substr(index, i - index) << endl;
+        cout << "Variable : " << text.substr(index, i - index) << endl; //Impresión de la variable
     return i;
 }
 
